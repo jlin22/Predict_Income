695e8fee5c30e37507179b2fd2ff253599be694f
unknown <jlin22@stuy.edu>
2018-06-30 13:32:41 -0400

    clean both files

--- clean.py
+++ clean.py
@@ -1,13 +1,21 @@
 import pandas as pd
 
-def clean(file_name):
-    test = pd.read_csv(file_name)
-    r, c = test.shape
-    for i in test.index:
-        if '?' in list(test[i:i+1]):
-            test.drop(i)
-    return test
+def clean(file_name, char):
+    if file_name == 'adult.test.txt':
+        df = pd.read_csv(file_name, skiprows = 1)
+    else:
+        df = pd.read_csv(file_name)
+    r, c = df.shape
+    for i in df.index:
+        if char in list(df[i:i+1]):
+            df.drop(i)
+    return df 
     
-test = clean("adult.data.txt")
+
+train = clean("adult.data.txt", '?')
+test = clean("adult.test.txt", '?')
+print('?' in train)
 print('?' in test)
+
+
 
